f17ddd7d47ef83d2a490a8f015b6ec063a5388f5
Shefat Al Mahmud <shefat2002@gmail.com>
2026-02-08 21:35:02 +0600

    added model

---
+++ TodoList/TodoList/Model/TaskModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Model;
+
+public class TaskModel
+{
+    public int Id { get; set; }
+
+    [StringLength(15, ErrorMessage = "Error! Name must be within 15 characters!")]
+    public string TaskTitle { get; set; } = null!;
+    
+    [StringLength(45, ErrorMessage = "Error! Description must be within 45 characters!")]
+    public string Description { get; set; } = null!;
+    public bool IsCompleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime DueDate { get; set; } = DateTime.UtcNow;
+
+}
